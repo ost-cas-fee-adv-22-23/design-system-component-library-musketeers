7fdce6ec8cb3a92a8533e01ca808d1e0015bf7b0
André Abt <info@andre-abt.com>
2022-12-01 16:51:06 +0100

    feat: 24 Add configuration, types and icons

--- src/components/button.stories.tsx
+++ src/components/button.stories.tsx
@@ -1,19 +1,21 @@
-import { ComponentMeta, ComponentStory } from '@storybook/react';
-import React from 'react';
-import { Button, ButtonType } from './button';
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import React from "react";
+import { Button, ButtonType } from "./button";
 
 export default {
-  title: 'Example/Button',
+  title: "Example/Button",
   component: Button,
   argTypes: {
     type: {
-      options: ['primary', 'secondary'],
-      control: { type: 'select' },
+      options: ["primary", "secondary"],
+      control: { type: "select" },
     },
   },
 } as ComponentMeta<typeof Button>;
 
-const Template: ComponentStory<typeof Button> = (args) => <Button {...args}>Hello World</Button>;
+const Template: ComponentStory<typeof Button> = (args) => (
+  <Button {...args}>Hello World</Button>
+);
 
 export const ButtonComponent = Template.bind({});
 ButtonComponent.args = {
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{ts,tsx}"],
+  content: ["./src/**/*.{ts,tsx,mdx}"],
   presets: [],
   darkMode: "media", // or 'class'
   theme: {
