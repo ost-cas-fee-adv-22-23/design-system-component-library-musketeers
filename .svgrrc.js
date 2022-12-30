56b5e0089c982ecc7a4ff322a8b1d2eb53da3807
André Abt <info@andre-abt.com>
2022-12-30 10:26:45 +0100

    fix: Change cli version of SVGR to 5.5

--- .svgrrc.js
+++ .svgrrc.js
@@ -1,4 +1,24 @@
 module.exports = {
   icon: true,
   replaceAttrValues: { '#475569': 'currentColor' },
+  template: function defaultTemplate({ template }, opts, { imports, interfaces, componentName, props, jsx, exports }) {
+    const plugins = ['jsx'];
+
+    if (opts.typescript) {
+      plugins.push('typescript');
+    }
+
+    const typeScriptTpl = template.smart({ plugins });
+
+    return typeScriptTpl.ast`
+      ${imports}
+
+      ${interfaces}
+
+      function ${componentName}(${props}) {
+        return ${jsx};
+      }
+      ${exports}
+    `;
+  },
 };
