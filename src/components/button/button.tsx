55db4ad20283b2559426c18d16223ef3da1b4621
André Abt <info@andre-abt.com>
2023-01-03 21:11:39 +0100

    feat: #37 Use Button for edit icon

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { ButtonProps, ButtonSize, ButtonType } from './button.types';
 
-export const Button: React.FC<ButtonProps> = (props) => {
-  const baseClasses = ['flex items-center h-fit text-white rounded-default border-4 border-white'];
-  const iconClasses =
-    'border-4 border-white text-white rounded-38 p-m bg-slate-600 hover:bg-slate-700 hover:border-slate-100 focus:border-slate-200';
+export const Button: React.FC<ButtonProps> = ({ showBorder = true, ...props }) => {
+  const baseClasses = ['flex items-center h-fit text-white rounded-default'];
+  const iconClasses = [
+    'text-white rounded-38 bg-slate-600 hover:bg-slate-700 hover:border-slate-100 focus:border-slate-200',
+  ];
 
   if (props.type === ButtonType.DEFAULT) {
     baseClasses.push('bg-slate-600 hover:bg-slate-700 hover:border-slate-100 focus:border-slate-200');
@@ -18,16 +19,31 @@ export const Button: React.FC<ButtonProps> = (props) => {
     baseClasses.push('bg-gradient-to-r from-pink-500 to-violet-600 hover:border-violet-100 focus:border-violet-200');
   }
 
+  if (props.size === ButtonSize.S) {
+    baseClasses.push('px-xs py-xxs');
+    iconClasses.push('p-s');
+  }
+
   if (props.size === ButtonSize.M) {
     baseClasses.push('px-s py-xs');
+    iconClasses.push('p-m');
   }
 
   if (props.size === ButtonSize.L) {
     baseClasses.push('px-m py-s');
+    iconClasses.push('p-m');
+  }
+
+  if (showBorder) {
+    baseClasses.push('border-4 border-white');
   }
 
   return (
-    <button aria-label={props.label} className={props.isIconOnly ? iconClasses : baseClasses.join(' ')}>
+    <button
+      onClick={props.onClick}
+      aria-label={props.label}
+      className={props.isIconOnly ? iconClasses.join(' ') : baseClasses.join(' ')}
+    >
       <span hidden={props.isIconOnly}>{props.label}</span>
       <span className={props.isIconOnly ? '' : 'pl-xs'}>{props.children}</span>
     </button>
