3371a0abc290002943c7d414088d43a1ac14d792
André Abt <info@andre-abt.com>
2023-01-03 19:55:40 +0100

    fix: typings

--- src/components/avatar/avatar.stories.tsx
+++ src/components/avatar/avatar.stories.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { StorybookMeta } from '../components.config';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
 import { Avatar } from './avatar';
-import { AvatarProps, AvatarSize } from './avatar.types';
+import { AvatarSize } from './avatar.types';
 
 export default {
   title: `${StorybookMeta.COMPONENTS}/Avatar`,
@@ -21,13 +22,13 @@ export default {
       control: { type: 'boolean' },
     },
   },
-};
+} as ComponentMeta<typeof Avatar>;
 
-const withSingleAvatar = (args: AvatarProps) => {
+const withSingleAvatar: ComponentStory<typeof Avatar> = (args) => {
   return <Avatar src={args.src} alt={args.alt} size={args.size} showBorder={args.showBorder}></Avatar>;
 };
 
-export const WithSingleAvatar: { (args: AvatarProps): JSX.Element; args?: AvatarProps } = withSingleAvatar.bind({});
+export const WithSingleAvatar: ComponentStory<typeof Avatar> = withSingleAvatar.bind({});
 WithSingleAvatar.args = {
   src: 'https://randompicturegenerator.com/img/people-generator/gd121f56d8674f28d00ce9f1c44686e7a9bee58b8d33a3c57daaada1fa493c214290f9490833d1ff18f4ee16cd5298e1f_640.jpg',
   alt: '',
