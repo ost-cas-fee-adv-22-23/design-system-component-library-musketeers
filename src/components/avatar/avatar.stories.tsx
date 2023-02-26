d29fb04ae88077fda50b9412ff6cd7330ac7b943
samirmumic <sam.mumic@gmail.com>
2023-02-26 13:47:32 +0100

    feat: #77 adds navigationitem link variation for nextjs

--- src/components/avatar/avatar.stories.tsx
+++ src/components/avatar/avatar.stories.tsx
@@ -38,3 +38,13 @@ AvatarComponent.args = {
   size: AvatarSize.M,
   showBorder: false,
 };
+
+export const AvatarAsAnchor: ComponentStory<typeof Avatar> = Template.bind({});
+AvatarAsAnchor.args = {
+  src: 'https://randompicturegenerator.com/img/people-generator/gd121f56d8674f28d00ce9f1c44686e7a9bee58b8d33a3c57daaada1fa493c214290f9490833d1ff18f4ee16cd5298e1f_640.jpg',
+  alt: '',
+  size: AvatarSize.M,
+  showBorder: false,
+  href: '/',
+  elementType: 'a',
+};
