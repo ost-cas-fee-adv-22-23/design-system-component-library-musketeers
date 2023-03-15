be055669e16d6fd891a8ff7902a7effc00d311ac
André Abt <info@andre-abt.com>
2023-03-15 10:47:18 +0100

    feat: #90 Hashtag size

--- src/components/hashtag/hashtag.types.ts
+++ src/components/hashtag/hashtag.types.ts
@@ -1,6 +1,12 @@
 import { MouseEvent } from 'react';
 
+export enum HashtagSize {
+  M = 'm',
+  L = 'l',
+}
+
 export type HashtagProps = {
   label?: string;
+  size?: HashtagSize;
   onClick: (e: MouseEvent<HTMLButtonElement>) => void;
 };
