c1f0c504e2e12f1969ecb83b56def75156d3a7d6
André Abt <info@andre-abt.com>
2023-04-24 12:24:48 +0200

    feat: #0 Add IconLink variants

--- src/components/icon-link/icon-link.tsx
+++ src/components/icon-link/icon-link.tsx
@@ -2,14 +2,26 @@ import React from 'react';
 import { IconLinkProps, IconLinkType } from './icon-link.types';
 
 export const IconLink: React.FC<IconLinkProps> = (props) => {
-  const baseClasses = ['flex items-center h-fit text-14 font-semibold cursor-pointer transition'];
+  const baseClasses = ['flex items-center h-fit text-14 font-semibold'];
+
+  if (props.href || props.onClick) {
+    baseClasses.push('cursor-pointer transition');
+  }
+
+  if ((props.href || props.onClick) && props.type === IconLinkType.DEFAULT) {
+    baseClasses.push('hover:text-slate-600');
+  }
+
+  if ((props.href || props.onClick) && props.type === IconLinkType.VIOLET) {
+    baseClasses.push('hover:text-violet-900');
+  }
 
   if (props.type === IconLinkType.DEFAULT) {
-    baseClasses.push('text-slate-400 hover:text-slate-600');
+    baseClasses.push('text-slate-400');
   }
 
   if (props.type === IconLinkType.VIOLET) {
-    baseClasses.push('text-violet-600 hover:text-violet-900');
+    baseClasses.push('text-violet-600');
   }
 
   return (
